96cc7e3cbe135b2280740bebba7197b2432e7c1e
Maxime <maxime.boehm@gmail.com>
2023-03-30 10:46:54 +0200

    Commit_20230330_carrousel

---
+++ app/src/main/java/fr/isen/maxime/androideresstaurant/CardsActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.maxime.androideresstaurant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cards)
+    }
+}
--- app/src/main/java/fr/isen/maxime/androideresstaurant/DetailActivity.kt
+++ app/src/main/java/fr/isen/maxime/androideresstaurant/DetailActivity.kt
@@ -1,5 +1,6 @@
 package fr.isen.maxime.androideresstaurant
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -20,7 +21,10 @@ class DetailActivity : AppCompatActivity() {
     private  var totalPrice : Float = 0F
     private  var itemCounter : Int = 1
     private  var price : Float = 0F
+    private  var cardNb : Int = 0
 
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
@@ -48,6 +52,12 @@ class DetailActivity : AppCompatActivity() {
             calculAndPrint()
         }
 
+        findViewById<Button>(R.id.addToCard).setOnClickListener {
+            cardNb += itemCounter
+            findViewById<TextView>(R.id.cardNumber).text = cardNb.toString()
+        }
+
+
         //Construit la liste d'ingrédients
         val listeIngredients = itemFromLauchActivity.ingredients
         val construction = StringBuilder()
@@ -60,49 +70,63 @@ class DetailActivity : AppCompatActivity() {
         findViewById<TextView>(R.id.ingredients).text = construction.toString()
 
         //ViewPager en cours d'écriture
-        /*val viewPager = findViewById<ViewPager>(R.id.viewPager2)
+        val viewPager = findViewById<ViewPager>(R.id.imagePager)
         val adapter = ImagePagerAdapter(supportFragmentManager, itemFromLauchActivity.images)
-        viewPager.adapter = adapter*/
+        viewPager.adapter = adapter
 
     }
 
     fun calculAndPrint(){
+
         totalPrice = itemCounter*price
         findViewById<TextView>(R.id.quantity).text = itemCounter.toString()
+        findViewById<TextView>(R.id.cardNumber).text = cardNb.toString()
         findViewById<Button>(R.id.addToCard).text = "Add to card : " + totalPrice.toString() + "€"
     }
 
-
-}
-
-//ViewPager en cours d'écriture
-/*class ImagePagerAdapter(fm: FragmentManager, private val images: List<String>) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int = images.size
-    override fun getItem(position: Int): Fragment {
-        return ImageFragment.newInstance(images[position])
-    }
-}
-class ImageFragment : Fragment() {
-    companion object {
-        private const val ARG_IMAGE_URL = "image_url"
-
-        fun newInstance(imageUrl: String): ImageFragment {
-            val args = Bundle()
-            args.putString(ARG_IMAGE_URL, imageUrl)
-            val fragment = ImageFragment()
-            fragment.arguments = args
-            return fragment
+    //ViewPager en cours d'écriture
+    class ImagePagerAdapter(fm: FragmentManager, private val images: List<String>) : FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+        override fun getCount(): Int = images.size
+        override fun getItem(position: Int): Fragment {
+            return ImageFragment.newInstance(images[position])
         }
     }
+    class ImageFragment : Fragment() {
+        companion object {
+            private const val ARG_IMAGE_URL = "image_url"
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val imageView = ImageView(context)
-        imageView.scaleType = ImageView.ScaleType.CENTER_INSIDE
-        val imageUrl = arguments?.getString(ARG_IMAGE_URL)
-        if(imageUrl!=null && imageUrl.isNotEmpty()){
-            Picasso.get().load(imageUrl).into(imageView)}
-        return imageView
+            fun newInstance(imageUrl: String): ImageFragment {
+                val args = Bundle()
+                args.putString(ARG_IMAGE_URL, imageUrl)
+                val fragment = ImageFragment()
+                fragment.arguments = args
+                return fragment
+            }
+        }
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            return inflater.inflate(R.layout.image_fragment, container,false)
+        }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            val imageUrl = arguments?.getString(ARG_IMAGE_URL)
+            val imageView = view.findViewById<ImageView>(R.id.fragmentImage)
+
+            if (imageUrl != null && imageUrl.isNotEmpty()) {
+                Picasso.get()
+                    .load(imageUrl)
+                    .fit()
+                    .centerCrop()
+                    .error(R.drawable.light)
+                    .into(imageView)
+            } else {
+                imageView.setImageResource(R.drawable.light)
+            }
+        }
+
     }
 
 
-}*/
+}
+
